21b4927e1285458dd99420c0039d15a1af8ff2cf
zb <1826992383@qq.com>
2018-11-02 20:11:16 +0800

    表单校验和多文件上传完成

--- public/back/js/product.js
+++ public/back/js/product.js
@@ -2,6 +2,8 @@ $(function () {
   // 一进来先发送ajax请求，动态渲染商品列表和分页
   var currentPage = 1;
   var pageSize = 2;
+
+  var picArr = [];
   // 进来先调用一次
   render();
   function render() {
@@ -39,26 +41,186 @@ $(function () {
 
   //点击添加分类按钮
   $('.addCategory').click(function () {
-      //模态框显示
-      $('#addModal').modal('show');
-      //发送ajax请求渲染二级分类列表
-      $.ajax({
-          type: 'get',
-          url : '/category/querySecondCategoryPaging',
-          data : {
-            page : 1,
-            pageSize : 100
-          },
-          dataType : 'json',
-          success : function (info) {
-            console.log(info); //准备数据
-            //准备模板
-            var htmlStr2 = template ('dropDownTpl',info);
-            //将数据渲染到页面上
-            $('.dropdown-menu').html(htmlStr2);            
-            
-          }
-      })
+    //模态框显示
+    $('#addModal').modal('show');
+    //发送ajax请求渲染二级分类列表
+    $.ajax({
+      type: 'get',
+      url: '/category/querySecondCategoryPaging',
+      data: {
+        page: 1,
+        pageSize: 100
+      },
+      dataType: 'json',
+      success: function (info) {
+        console.log(info); //准备数据
+        //准备模板
+        var htmlStr2 = template('dropDownTpl', info);
+        //将数据渲染到页面上
+        $('.dropdown-menu').html(htmlStr2);
+
+      }
+    })
   })
+
+  //点击分类，选中对应的分类，改变按钮文本
+  //需要注册事件委托
+  $('.dropdown-menu').on('click', 'a', function () {
+    //获取二级分类文本
+    var txt = $(this).text();
+    // 设置给按钮
+    $('#dropdownMenu1').text(txt);
+    //获取当前二级分类的id
+    var id = $(this).data('id');
+    //设置给隐藏域
+    $('[name="brandId"]').val(id);
+    //此时要手动更改隐藏域校验状态
+    $('#form').data('bootstrapValidator').updateStatus('brandId', 'VALID')
+  })
+
+  // 多文件上传
+  $('#fileupload').fileupload({
+    dataType: 'json',
+    done: function (e, data) {
+      console.log(data.result);
+      //准备数据
+      var picObj = data.result;
+      var picAddr = picObj.picAddr;  //获取图片路径
+
+      //一方面把地址存到数组中(数据存储层面)
+      picArr.unshift(picObj);
+      //另一方面将页面中添加img元素,并且每次都添加在最前面(页面渲染方面)
+      $('#img_box').prepend('<img height="100" src="' + picAddr + '"></img>');
+
+      //判断数组的长度是否大于3
+      if (picArr.length > 3) {
+        //删除数组的最后一项
+        picArr.pop();
+        //页面上删除最后一个img标签
+        $('#img_box img:last-of-type').remove();
+      }
+
+      // 判断数组的长度，若长度等于3，则手动改变隐藏域的校验状态
+      if (picArr.length === 3) {
+        $('#form').data('bootstrapValidator').updateStatus('picStatus', 'VALID')
+      }
+
+
+    }
+
+  })
+
+
+
+  //添加表单校验
+  $('#form').bootstrapValidator({
+    //1. 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)'],可以不设置
+    excluded: [],
+
+    //2. 指定校验时的图标显示，默认是bootstrap风格
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+
+    //3、校验字段
+    fields: {
+      //校验二级分类名
+      brandId: {
+        //校验规则
+        validators: {
+          notEmpty: {
+            message: '请选择二级分类'
+          }
+        }
+      },
+      proName: {
+        //校验规则
+        validators: {
+          notEmpty: {
+            message: '请输入商品名称'
+          }
+        }
+      },
+      proDesc: {
+        //校验规则
+        validators: {
+          notEmpty: {
+            message: '请输入商品描述'
+          }
+        }
+      },
+      num: {
+        //校验规则
+        validators: {
+          notEmpty: {
+            message: '请输入商品库存'
+          },
+          //正则校验
+          regexp: {
+            regexp: /^[1-9]\d*$/,
+            message: '库存格式要求是非零开头的数字'
+          }
+        }
+      },
+      size: {
+        //校验规则
+        validators: {
+          notEmpty: {
+            message: '请输入商品尺码'
+          },
+          //正则校验
+          regexp: {
+            regexp: /^\d{2}-\d{2}$/,
+            message: '尺码格式必须是 xx-xx 的格式, 例如: 32-40'
+          }
+        }
+      },
+      oldPrice: {
+        //校验规则
+        validators: {
+          notEmpty: {
+            message: '请输入商品原价'
+          },
+          //正则校验
+          regexp: {
+            regexp: /^[1-9]\d*$/,
+            message: '商品价格必须是非0开头的数字'
+          }
+        }
+      },
+      price: {
+        //校验规则
+        validators: {
+          notEmpty: {
+            message: '请输入商品现价'
+          },
+          //正则校验
+          regexp: {
+            regexp: /^[1-9]\d*$/,
+            message: '商品价格必须是非0开头的数字'
+          }
+        }
+      },
+      picStatus: {
+        //校验规则
+        validators: {
+          notEmpty: {
+            message: '请输入3张图片'
+          }
+        }
+      }
+
+    }
+
+  })
+
+
+
+
+
+
+
 
 })
