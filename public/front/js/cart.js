a50ee91a48344b50320332196d6c65b998f54f73
zb <1826992383@qq.com>
2018-11-05 22:51:06 +0800

    购物车动态渲染完成

--- public/front/js/cart.js
+++ public/front/js/cart.js
@@ -1,11 +1,20 @@
 $(function () {
-  // 一进来先渲染
+  // 功能1：一进来先渲染
   $.ajax({
     type: 'get',
     url: '/cart/queryCart',
     dataType: 'json',
-    success: function () {
-      
+    success: function (info) {
+      console.log(info);
+      //如果没有登录，直接跳转到登录页去登录
+      if (info.error === 400) {
+        location.href = 'login.html';
+        return;
+      }
+      //登录成功，直接渲染页面
+      var htmlStr = template ('cartTpl',{list:info})
+      //渲染数据到页面上
+      $('.lt_main .mui-table-view').html(htmlStr);
     }
   })
 })
--- public/front/js/user.js
+++ public/front/js/user.js
@@ -1,9 +1,36 @@
 $(function () {
-  // 进来先动态渲染页面
+  // 功能1：进来先动态渲染页面
   $.ajax({
     type: 'get',
     url: '/user/queryUserMessage',
-    dataType : 'json',
-    succes
+    dataType: 'json',
+    success: function (info) {
+      console.log(info);
+      //若果没有登录，直接跳到登录页
+      if (info.error === 400) {
+        location.href = 'login.html';
+        return;
+      }
+      // 将模板跟对象绑定
+      var htmlStr = template('userTpl', info);
+      //将数据渲染到页面上
+      $('#userInfo').html(htmlStr);
+    }
+  })
+
+  //功能2：点击退出按钮，发送ajax请求，清除登录状态，回到登录页
+  $('#logout').click(function () {
+    $.ajax({
+      type: 'get',
+      url: '/user/logout',
+      dataType: 'json',
+      success: function (info) {
+        // console.log(info);
+        if (info.success) {
+          location.href = 'login.html'; //跳转到登录页
+        }
+
+      }
+    })
   })
 })
